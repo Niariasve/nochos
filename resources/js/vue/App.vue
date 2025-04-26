410ff62530ea5932d44099827f7f7fb1d356fd7f
Niariasve <niarias@espol.edu.ec>
2025-04-25 22:16:02 -0500

    WIP: configuracion incial de vue

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="text-sm">Buenas tardes mi gente</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
---
+++ resources/views/Vue/vue.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue</title>
+
+    @vite(['resources/js/vue/main.js'])
+</head>
+<body>
+    <div>
+        <div id="app"></div>
+    </div>
+</body>
+</html>
